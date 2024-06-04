4408d9d99ca050ec58458404635c4d1b2d664164
Centri3 <114838443+Centri3@users.noreply.github.com>
2024-06-04 14:16:12 -0500

    prominences!

--- shaders/corona_plane_proc.glsl
+++ shaders/corona_plane_proc.glsl
@@ -3,91 +3,142 @@
 
 #include "libcst_shared.glh"
 
-//uniform vec3      Color;
-//uniform vec4      Bright; // (Gamma, Brightness, RayBrightness, SpotBrightness)
-//uniform vec4      Shape;  // (RayDensity, RayCurvature, Scale, SinFreq)
-//uniform vec4      EyePos; // (eye pos, animation time)
-//uniform mat4x4    Mvp;
+struct BrightTy {
+    float gamma;
+    float brightness;
+    float ray_brightness;
+    float spot_brightness;
+};
+
+struct ShapeTy {
+    float ray_density;
+    float ray_curvature;
+    float scale;
+    float sin_freq;
+};
+
+struct EyeTy {
+    vec3 pos;
+    float anim_time;
+};
+
 #uniform_block
 
 #ifdef _VERTEX_
 
-layout(location = 0) in  vec4  VertexPos;
-layout(location = 1) in  vec4  VertexTexCoord;
-                     out vec2  TexCoord;
-                     out vec3  PixPos;
+layout(location = 0) in vec4 in_vert_pos;
+layout(location = 1) in vec4 in_vert_tex_coord;
+out vec2 out_tex_coord;
+out vec3 out_pix_pos;
 
-void main()
-{
-    gl_Position = Mvp * VertexPos;
-    TexCoord = VertexTexCoord.xy;
-    PixPos = VertexPos.xyz;
+void main() {
+    gl_Position = Mvp * in_vert_pos;
+    out_tex_coord = in_vert_tex_coord.xy;
+    out_pix_pos = in_vert_pos.xyz;
 }
 
 #else
 
-                     in  vec2  TexCoord;
-                     in  vec3  PixPos;
-layout(location = 0) out vec4  OutColor;
+in vec2 out_tex_coord;
+in vec3 out_pix_pos;
+layout(location = 0) out vec4 out_color;
 
-float noise(in vec2 x)
-{
-	vec2 p = floor(x);
-    vec2 f = fract(x);
-    f = f * f * (3.0 - 2.0 * f);
-    float  n = p.x + p.y * 57.0;
-    return mix(mix(hash(n),        hash(n +  1.0), f.x),
-               mix(hash(n + 57.0), hash(n + 58.0), f.x), f.y);
-}
-
-const mat2 m2 = mat2(1.60,  1.20, -1.20, 1.60);
-
-float fbm(in vec2 p)
-{	
-	float z = 0.5;
-	float rezult = 0.0;
-	for (int i = 1; i < 6; i++)
-	{
-		rezult += (sin(noise(p) * Shape.w) * 0.5 + 0.5) * z;
-		z *= 0.5;
-		p *= m2;
-	}
-	return rezult;
-}
+#define in_tex_coord out_tex_coord
+#define in_pix_pos out_pix_pos
 
 void main(void)
 {
-    vec2 uv0 = TexCoord * 2.0 - 1.0;
-    vec2 uv  = uv0 * Shape.y * 0.05;
+    vec3 _color = Color;
+    BrightTy bright = BrightTy(Bright.x, Bright.y, Bright.z, Bright.w);
+    ShapeTy shape = ShapeTy(Shape.x, Shape.y, Shape.z, Shape.w);
+    EyeTy eye = EyeTy(vec3(EyePos.x, EyePos.y, EyePos.z), EyePos.w);
+
+    vec2 uv0 = in_tex_coord * 2.0 - 1.0;
+    vec2 uv = uv0 * shape.ray_curvature * 0.05;
 	vec2 uvn = normalize(uv);
 
     float r0 = sqrt(dot(uv0, uv0));
-    float r  = sqrt(dot(uv,  uv));
-    float x = dot(uvn, vec2(0.5, 0.0)) - EyePos.w;
-    float y = dot(uvn, vec2(0.0, 0.5)) - EyePos.w;
+    float r = sqrt(dot(uv, uv));
+    float x = dot(uvn, vec2(0.5, 0.0)) - eye.anim_time;
+    float y = dot(uvn, vec2(0.0, 0.5)) - eye.anim_time;
 
-    float val;
-    val = fbm(Shape.z * vec2(r + y * Shape.x, r + x * Shape.x - y));
-    val = smoothstep(Bright.x, Bright.x + Bright.z, val);
-    val = sqrt(val);
+    float rays = noise_fbm_float(
+        vec3(
+            (r + x) / 60.0 * shape.ray_density,
+            (r + y) / 60.0 * shape.ray_density,
+            0.0
+        ),
+        NoiseParams(2.6f, 8.0f, 0.2f, 0.0f, 0.0001, 0.1, 0.55)
+    );
 
-	vec3 color = vec3(val);// / max(Color, 0.2);
-    color = mix(1.0 - color, vec3(1.0), Bright.w - 2000.0 * r / (Shape.y * Bright.y));
+    rays = smoothstep(bright.gamma, bright.gamma + bright.ray_brightness, rays);
+    rays = sqrt(rays);
 
-    vec3  ray  = PixPos + EyePos.xyz;
+    float eucl_dist = sqrt(pow(in_pix_pos.x, 2.0f)
+        + pow(in_pix_pos.y, 2.0f)
+        + pow(in_pix_pos.z, 2.0f)
+    );
+
+    vec3 color = vec3(rays);
+    color += mix(
+        1.0f - color.rgb,
+        vec3(0.95f),
+        bright.spot_brightness - 2000.0 * r / (shape.ray_curvature * bright.brightness)
+    ) * 1.8f;
+    color *= _color;
+    color += vec3(0.3 * (_color.r + _color.g + _color.b));
+    color *= saturate(0.74f - eucl_dist);
+
+    uv  = uv0 * 36.0f * 0.05;
+	uvn = normalize(uv);
+
+    r0 = sqrt(dot(uv0, uv0));
+    r = sqrt(dot(uv, uv));
+    x = dot(uvn, vec2(0.5, 0.0)) - eye.anim_time * 1.1111f;
+    y = dot(uvn, vec2(0.0, 0.5)) - eye.anim_time * 1.1111f;
+
+    float prominences = noise_fbm_float(
+        vec3(
+            (r + x) / 60.0 * shape.ray_density,
+            (r + y) / 60.0 * shape.ray_density,
+            0.0
+        ),
+        NoiseParams(3.2f, 8.0f, 0.4f, 0.0f, 0.0001, 0.1, 0.55)
+    );
+
+    // Doing this 3 times allows prominences to be brighter near the middle. There's probably a
+    // better way to do this, but I'm inexperienced.
+
+    color.rgb += saturate(
+        mix(
+            1.0f - vec3(prominences),
+            vec3(0.5f),
+            bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
+        ) * _color
+    ) * 0.2f;
+    color.rgb += saturate(
+        mix(
+            1.0f - vec3(prominences),
+            vec3(0.67f),
+            bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
+        ) * _color
+    ) * 1.3f;
+    color.rgb += saturate(
+        mix(
+            1.0f - vec3(prominences),
+            vec3(0.73f),
+            bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
+        ) * _color
+    ) * 9.3f;
+
+    vec3 ray = in_pix_pos + eye.pos;
     float dist = length(ray);
-    float fade = clamp(dist - 0.05, 0.0, 1.0) * clamp(1.0 - r0, 0.0, 1.0) *
-                 clamp(abs(ray.z / dist) - 0.1, 0.0, 1.0);
 
-	uv *= m2;
+    float fade = clamp(dist - 0.01f, 0.0f, 1.0f)
+        * clamp(1.0 - r0, 0.0, 1.0)
+        * clamp(abs(ray.z / dist) - 0.1, 0.0, 1.0);
 
-    OutColor.rgb = fade * (Color * max(color, 0.0));
-
-    // gamma correction
-    OutColor.rgb = pow(OutColor.rgb, vec3(2.2));
-
-	OutColor.a = 0.0;
+    out_color.rgb = fade * pow(max(color.rgb, 0.0), vec3(2.2f));
 }
 
 #endif
-
